a786cf160eb603ba54995353a475af66c918c864
Kashan-2912 <mkashan2912@gmail.com>
2025-08-13 18:21:46 +0500

    gtm removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -84,33 +84,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-      <head>
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm-script" strategy="afterInteractive">{
-            `(function(w,d,s,l,i){w[l]=w[l]||[];
-          w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-          var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-          j.async=true;
-          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-          f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MQNQ2SZ6');`
-          }
-        </Script>
-        {/* End Google Tag Manager */}
-      </head>
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${greatVibes.variable} ${poppins.variable} ${cinzel.variable} ${workSans.variable} ${inter.variable} ${helveticaNeue.variable} ${gravesendSans.variable} antialiased overflow-x-hidden`}
       >
-
-        {/* <!-- Google Tag Manager (noscript) --> */}
-
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQNQ2SZ6"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
-        {/* <!-- End Google Tag Manager (noscript) --> */}
 
         <div className="flex flex-col min-h-screen w-full max-w-full overflow-x-hidden">
           <Navbar />
