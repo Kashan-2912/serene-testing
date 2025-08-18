8f87758f1d8ace6453c587a83e8d858417fdc4c9
Kashan-2912 <mkashan2912@gmail.com>
2025-08-18 12:18:27 +0500

    fixed maps init multiple times

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useRef } from 'react';
+import Script from 'next/script';
 import markerPng from "../../../public/assets/marker.png"
 
 // Your Snazzy Maps style array
@@ -398,80 +399,82 @@ export default function SimpleMap() {
   const mapRef = useRef(null);
   const mapInstanceRef = useRef(null);
 
-  useEffect(() => {
-    if (!window.google) {
-      const script = document.createElement('script');
-      script.src =  `https://maps.googleapis.com/maps/api/js?key=AIzaSyBhNxQHyvty5jmoon23zVKcALKtAw-WXco&libraries=places`;
-      script.onload = initMap;
-      document.head.appendChild(script);
-    } else {
-      initMap();
+  function initMap() {
+    if (!mapInstanceRef.current && window.google) {
+      const location = { lat: 34.064834, lng: 73.366237 };
+
+      mapInstanceRef.current = new window.google.maps.Map(mapRef.current, {
+        center: location,
+        zoom: 16,
+        styles: customMapStyle,
+        disableDefaultUI: false,
+        zoomControl: true,
+        mapTypeControl: false,
+        streetViewControl: false,
+        fullscreenControl: false,
+      });
+
+      // Add custom marker
+      const marker = new window.google.maps.Marker({
+        position: location,
+        map: mapInstanceRef.current,
+        icon: {
+          url: markerPng.src,
+          scaledSize: new window.google.maps.Size(40, 40)
+        }
+      });
+
+      // Always-visible label
+      const labelDiv = document.createElement("div");
+      labelDiv.style.background = "transparent";
+      labelDiv.style.borderRadius = "6px";
+      labelDiv.style.fontSize = "16px";
+      labelDiv.style.fontWeight = "600";
+      labelDiv.style.color = "black";
+      labelDiv.style.textAlign = "left";
+      labelDiv.innerText = `Serene Heights Nathia\nGali Hotel & Resort`;
+
+      const labelOverlay = new window.google.maps.OverlayView();
+      labelOverlay.onAdd = function () {
+        const panes = this.getPanes();
+        panes.floatPane.appendChild(labelDiv);
+      };
+      labelOverlay.draw = function () {
+        const projection = this.getProjection();
+        const pos = projection.fromLatLngToDivPixel(location);
+        labelDiv.style.position = "absolute";
+        labelDiv.style.left = pos.x + 20 + "px";
+        labelDiv.style.top = pos.y - 35 + "px";
+      };
+      labelOverlay.onRemove = function () {
+        if (labelDiv.parentNode) {
+          labelDiv.parentNode.removeChild(labelDiv);
+        }
+      };
+      labelOverlay.setMap(mapInstanceRef.current);
     }
+  }
 
-    function initMap() {
-      if (!mapInstanceRef.current && window.google) {
-        const location = { lat: 34.064834, lng: 73.366237 };
-
-        mapInstanceRef.current = new window.google.maps.Map(mapRef.current, {
-          center: location,
-          zoom: 16, // zoomed out
-          styles: customMapStyle,
-          disableDefaultUI: false,
-          zoomControl: true,
-          mapTypeControl: false,
-          streetViewControl: false,
-          fullscreenControl: false
-        });
-
-        // Add custom marker
-        const marker = new window.google.maps.Marker({
-          position: location,
-          map: mapInstanceRef.current,
-          icon: {
-            url: markerPng.src,
-            scaledSize: new window.google.maps.Size(40, 40)
-          }
-        });
-
-        // Always-visible label
-        const labelDiv = document.createElement("div");
-        labelDiv.style.background = "transparent";
-        labelDiv.style.borderRadius = "6px";
-        labelDiv.style.fontSize = "16px";
-        labelDiv.style.fontWeight = "600";
-        labelDiv.style.color = "black";
-        labelDiv.style.textAlign = "left";
-        labelDiv.innerText = `Serene Heights Nathia\nGali Hotel & Resort`;
-
-        const labelOverlay = new window.google.maps.OverlayView();
-        labelOverlay.onAdd = function () {
-          const panes = this.getPanes();
-          panes.floatPane.appendChild(labelDiv);
-        };
-        labelOverlay.draw = function () {
-          const projection = this.getProjection();
-          const pos = projection.fromLatLngToDivPixel(location);
-          labelDiv.style.position = "absolute";
-          labelDiv.style.left = pos.x + 20 + "px";
-          labelDiv.style.top = pos.y - 35 + "px";
-        };
-        labelOverlay.onRemove = function () {
-          if (labelDiv.parentNode) {
-            labelDiv.parentNode.removeChild(labelDiv);
-          }
-        };
-        labelOverlay.setMap(mapInstanceRef.current);
-      }
+  useEffect(() => {
+    if (window.google) {
+      initMap();
     }
   }, []);
 
   return (
-    <div
-      ref={mapRef}
-      style={{
-        width: '100%',
-        height: '100vh'
-      }}
-    />
+    <>
+      <Script
+        src={`https://maps.googleapis.com/maps/api/js?key=AIzaSyBhNxQHyvty5jmoon23zVKcALKtAw-WXco&libraries=places`}
+        strategy="afterInteractive"
+        onLoad={() => initMap()}
+      />
+      <div
+        ref={mapRef}
+        style={{
+          width: '100%',
+          height: '100vh'
+        }}
+      />
+    </>
   );
 }
