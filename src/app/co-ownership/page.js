63d54ba2052f745642aba1c1888b01b3d40c963d
Kashan-2912 <mkashan2912@gmail.com>
2025-08-19 11:52:15 +0500

    FeatureGrid section padding and responsiveness

--- src/app/co-ownership/page.js
+++ src/app/co-ownership/page.js
@@ -3,19 +3,18 @@ import FeaturesGrid from '@/components/coownership/FeaturesGrid';
 
 function Page() {
   return (
-    <div className="py-32 px-4 sm:px-6 lg:px-8">
+    <div className="py-32 px-7 md:px-12 lg:px-20">
       <div className="max-w-7xl mx-auto">
     
         <div className="text-left mb-10">
-          <p className="inter font-semibold text-[#37584F] mb-2 tracking-wide uppercase">
+          <p className="inter font-semibold text-xs md:text-xl text-[#37584F] mb-2 tracking-wide uppercase">
             WHY CHOOSE US
           </p>
-          <p className="text-3xl sm:text-5xl text-[#37584F] font-bold gravesendSans">
+          <p className="text-xl md:text-5xl text-[#37584F] font-bold gravesendSans">
             WHY SERENE HEIGHTS?
           </p>
         </div>
 
-        {/* Features Grid Component */}
         <FeaturesGrid />
       </div>
     </div>
