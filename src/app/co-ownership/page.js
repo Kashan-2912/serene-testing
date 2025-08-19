50e6b984cb1abd4c5cf41459e7663095771cae81
Kashan-2912 <mkashan2912@gmail.com>
2025-08-19 12:55:37 +0500

    Smart Property Section DONE

--- src/app/co-ownership/page.js
+++ src/app/co-ownership/page.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import FeaturesGrid from '@/components/coownership/FeaturesGrid';
+import SmartProperty from '@/components/coownership/SmartProperty';
 
 function Page() {
   return (
-    <div className="py-10 md:py-32 px-7 md:px-12 lg:px-20">
-      <div className="max-w-7xl mx-auto">
-    
-        <div className="text-left mb-10">
-          <p className="inter font-semibold text-xs md:text-xl text-[#37584F] mb-2 tracking-wide uppercase">
-            WHY CHOOSE US
-          </p>
-          <p className="text-xl md:text-5xl text-[#37584F] font-bold gravesendSans">
-            WHY SERENE HEIGHTS?
-          </p>
+    <div className="py-10 md:py-32 px-7 md:px-12">
+      <div className="max-w-7xl mx-auto flex flex-col gap-28">
+
+        <div>
+          <div className="text-left mb-10">
+            <p className="inter font-semibold text-xs md:text-xl text-[#37584F] mb-2 tracking-wide uppercase">
+              WHY CHOOSE US
+            </p>
+            <p className="text-xl md:text-5xl text-[#37584F] font-bold gravesendSans">
+              WHY SERENE HEIGHTS?
+            </p>
+          </div>
+
+          <FeaturesGrid />
         </div>
 
-        <FeaturesGrid />
+        <SmartProperty />
       </div>
     </div>
   );
