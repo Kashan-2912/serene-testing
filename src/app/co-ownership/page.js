cdbe968a033a75439699daf54c0f973390ce5f68
Kashan-2912 <mkashan2912@gmail.com>
2025-08-19 11:28:19 +0500

    FeatureGrid in Coownership DONE

--- src/app/co-ownership/page.js
+++ src/app/co-ownership/page.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React from 'react';
+import FeaturesGrid from '@/components/coownership/FeaturesGrid';
 
-function page() {
+function Page() {
   return (
-    <div className=" ">
-      <p className="sm:text-6xl text-4xl font-bold text-[#37584F] TE h-96  flex justify-center items-center gravesendSans">
-        COMING SOON
-      </p>
+    <div className="py-32 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto">
+    
+        <div className="text-left mb-10">
+          <p className="inter font-semibold text-[#37584F] mb-2 tracking-wide uppercase">
+            WHY CHOOSE US
+          </p>
+          <p className="text-3xl sm:text-5xl text-[#37584F] font-bold gravesendSans">
+            WHY SERENE HEIGHTS?
+          </p>
+        </div>
+
+        {/* Features Grid Component */}
+        <FeaturesGrid />
+      </div>
     </div>
   );
 }
 
-export default page;
+export default Page;
